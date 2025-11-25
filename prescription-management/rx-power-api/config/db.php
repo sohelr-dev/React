6f8e4d5375b41cd6915773a2d5ba54204c73ca7a
sohelr-dev <sohe.4273@gmail.com>
2025-11-26 01:17:10 +0600

    server and loacl config chaneg

--- prescription-management/rx-power-api/config/db.php
+++ prescription-management/rx-power-api/config/db.php
@@ -1,12 +1,19 @@
 <?php
 // Local
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'rx_power');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'rx_power');
 
 
-$db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-// $db->close();
+//server
+
+define("HOST","localhost");
+define("USER","sohelitc_sohel");
+define("PASSWORD","amisohel@715");
+define("DATABASE","sohelitc_rx-power");
+$db= new mysqli(HOST,USER,PASSWORD,DATABASE);
+
 ?>
--- prescription-management/src/config.tsx
+++ prescription-management/src/config.tsx
@@ -1,7 +1,14 @@
 import axios from "axios";
 
-const baseApiUrl ="http://localhost/rx-power-api/api/";
-const baseUrl ="http://localhost/rx-power-api/";
+//server
+
+const baseApiUrl="https://sohelit.com/rx-power-api/api/";
+const baseUrl ="https://sohelit.com/rx-power-api/";
+
+//local
+
+// const baseApiUrl ="http://localhost/rx-power-api/api/";
+// const baseUrl ="http://localhost/rx-power-api/";
 export {baseUrl};
 
 const api = axios.create({
